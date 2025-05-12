91d890e0359f5c39a1059c1426233f5f88349a75
Viktor Gavrilov <Gavr320@yandex.ru>
2025-05-12 21:48:59 +0300

    Реализован UserDbStorage и изменена логика в in-memory. Исправлен сервис для user

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,37 +1,37 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Component
+@Qualifier("InMemoryUserStorage")
 public class InMemoryUserStorage implements UserStorage {
     private final Map<Integer, User> users = new HashMap<>();
 
     @Override
-    public User add(User user) {
+    public User addUser(User user) {
         user.setId(idGenerate());
         users.put(user.getId(), user);
         return user;
     }
 
     @Override
-    public Collection<User> findAll() {
+    public Collection<User> getUsers() {
         return users.values();
     }
 
     @Override
-    public User update(User user) {
+    public User updateUser(User user) {
         users.put(user.getId(), user);
         return user;
     }
 
     @Override
-    public Optional<User> findById(Integer id) {
+    public Optional<User> findUserById(Integer id) {
         return Optional.ofNullable(users.get(id));
     }
 
@@ -40,5 +40,46 @@ public class InMemoryUserStorage implements UserStorage {
                 .stream()
                 .max(Integer::compareTo)
                 .orElse(0) + 1;
+    }
+
+    @Override
+    public void addFriend(int userId, int friendId) {
+        User user = getUserOrThrow(userId);
+        User friend = getUserOrThrow(friendId);
+
+        user.addFriend(friend.getId());
+        friend.addFriend(user.getId());
+    }
+
+    @Override
+    public void removeFriend(int userId, int friendId) {
+        User user = getUserOrThrow(userId);
+        User friend = getUserOrThrow(friendId);
+
+        user.removeFriend(friend.getId());
+        friend.removeFriend(user.getId());
+    }
+
+    @Override
+    public List<User> getFriends(int id) {
+        return getUserOrThrow(id).getFriends().stream()
+                .map(this::getUserOrThrow)
+                .toList();
+    }
+
+    @Override
+    public List<User> getCommonFriends(int userId, int otherId) {
+        User user = getUserOrThrow(userId);
+        User other = getUserOrThrow(otherId);
+
+        Set<Integer> common = new HashSet<>(user.getFriends());
+        common.retainAll(other.getFriends());
+        return common.stream().map(this::getUserOrThrow).toList();
+    }
+
+    public User getUserOrThrow(int id) {
+        return findUserById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id " + id + " не найден"));
+
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,14 +3,20 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
-    User add(User user);
+    User addUser(User user);
 
-    User update(User user);
+    User updateUser(User user);
 
-    Collection<User> findAll();
+    Collection<User> getUsers();
 
-    Optional<User> findById(Integer id);
+    Optional<User> findUserById(Integer id);
+
+    void addFriend(int userId, int friendId);
+    void removeFriend(int userId, int friendId);
+    List<User> getFriends(int userId);
+    List<User> getCommonFriends(int userId, int otherId);
 }
