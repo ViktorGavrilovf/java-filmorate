9cb0cf9649d39ad8f4ba0e69ce49366c6bde3989
karppow <tesserackt@yandex.ru>
2025-05-28 14:15:05 +0300

    Добавлена функциональность «Отзывы»

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -7,11 +7,18 @@ import java.util.Optional;
 
 public interface ReviewStorage {
     Review create(Review review);
+
     Review update(Review review);
+
     void delete(int id);
+
     Optional<Review> findById(int id);
+
     List<Review> findByFilmId(Integer filmId, int count);
+
     void addLike(int reviewId, int userId);
+
     void addDislike(int reviewId, int userId);
+
     void removeReaction(int reviewId, int userId);
 }
